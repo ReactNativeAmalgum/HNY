4741608408c4a832259f8c27e7a5321f5256158d
ReactNativeAmalgum <137068385+ReactNativeAmalgum@users.noreply.github.com>
2024-08-22 15:27:23 +0530

    22-8-24 (banner work)

--- src/Components/Banner/CarouselBanner.jsx
+++ src/Components/Banner/CarouselBanner.jsx
@@ -1,38 +1,43 @@
 import React, { useState } from "react";
-import Slider from "react-slick";
+import { Carousel } from "react-bootstrap";
+import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import "./banner.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Nav } from "react-bootstrap";
-import { FaArrowRight } from "react-icons/fa";
-import { Link } from "react-router-dom";
 
 const CarouselBanner = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const settings = {
-    vertical: true,
-    verticalSwiping: true,
-    beforeChange: (current, next) => setCurrentSlide(next),
-    dots: true,
-    infinite: true,
-    speed: 200,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
+    interval: 3000, // This replaces `autoplaySpeed` from react-slick
+    pause: 'hover', // Pause on hover
+    controls: true, // Show controls if needed
+    indicators: true, // Show indicators if needed
   };
 
   const moreAbout = () => {
     return (
       <div className="booknow-circle-cont">
         <Link to={"/contact"} style={{ textDecoration: "none" }}>
-          <div className="book-now">
+          <div className="banner-book-now">
             <span> Book Now</span>
           </div>
 
-          <div className="circle">
+          <div className="book-now-circle">
             <FaArrowRight />
+          </div>
+        </Link>
+      </div>
+    );
+  };
+
+  const moreAbout2 = () => {
+    return (
+      <div className="booknow-circle-cont">
+        <Link to={"/contact"} style={{ textDecoration: "none" }}>
+          <div className="banner-book-now2">
+            <span> Book Now</span>
           </div>
         </Link>
       </div>
@@ -42,43 +47,50 @@ const CarouselBanner = () => {
   const slides = [
     {
       id: 1,
-      img: "https://c4.wallpaperflare.com/wallpaper/973/1004/995/interior-design-room-carpets-couch-wallpaper-preview.jpg",
-      text: "1 Beautiful Living Solution",
-      text2: "Give your hone a new look",
+      img: "https://images.livspace-cdn.com/w:1440/dpr:1/plain/https://d3gq2merok8n5r.cloudfront.net/abhinav/ond-1634120396-Obfdc/jas-2024-1720241010-wAOzD/mbr-1721373677-rlhqT/mbr-23-1721373698-6QPrf.jpg",
+      text: "Beautiful Living Solution",
+      text2: "HNY is a design firm that brings dimension to the design create for you",
     },
     {
       id: 2,
-      img: "https://e1.pxfuel.com/desktop-wallpaper/650/461/desktop-wallpaper-luxury-house-interior-%E2%9D%A4-for-ultra-luxury-house.jpg",
-      text: "2 Beautiful Living Solution",
-      text2: "Give your hone a new look",
+      img: "https://images.livspace-cdn.com/w:1440/dpr:1/plain/https://d3gq2merok8n5r.cloudfront.net/abhinav/ond-1634120396-Obfdc/amj-2024-1711965337-VsMIK/mbr-1711965351-TMqEb/mbr17-1715845112-j1tZP.jpg",
+      text: "Beautiful Living Solution",
+      text2: "Give your home a new look with these interior design ideas create for you",
     },
     {
       id: 3,
-      img: "https://i.pinimg.com/originals/21/de/be/21debe76191a921dd71285ac1bf0e2dc.png",
-      text: "3 Beautiful Living Solution",
-      text2: "Give your hone a new look",
+      img: "https://images.livspace-cdn.com/w:1440/dpr:1/plain/https://d3gq2merok8n5r.cloudfront.net/abhinav/ond-1634120396-Obfdc/jas-2024-1720241010-wAOzD/mbr-1721373677-rlhqT/mbr-11-1721373690-sJ0m2.jpg",
+      text: "Beautiful Living Solution",
+      text2: "HNY is a design firm that brings dimension to the design create for you",
     },
   ];
 
   return (
     <div className="carousel-container">
+      <Carousel
+        data-bs-theme="dark"
+        onSelect={(selectedIndex) => setCurrentSlide(selectedIndex)}
+        interval={settings.interval}
+        pause={settings.pause}
+        controls={settings.controls}
+        indicators={settings.indicators}
+      >
+        {slides.map((slide) => (
+          <Carousel.Item key={slide.id}>
+            <img
+              className="d-block w-100 slider-img"
+              src={slide.img}
+              alt={`Slide ${slide.id}`}
+            />
+          </Carousel.Item>
+        ))}
+      </Carousel>
       <div className="text-box">
         <h3 className="txt-h3">{slides[currentSlide].text}</h3>
         <h2>{slides[currentSlide].text2}</h2>
-        {moreAbout()}
+        <div className="moreAbout">{moreAbout()}</div>
+        <div className="moreAbout2">{moreAbout2()}</div>
       </div>
-      <Slider {...settings}>
-        {slides.map((slide) => (
-          <div key={slide.id}>
-            <div></div>
-            <img
-              src={slide.img}
-              alt={`Slide ${slide.id}`}
-              className="slide-image"
-            />
-          </div>
-        ))}
-      </Slider>
     </div>
   );
 };
--- src/Page/services pages/Design_planning.jsx
+++ src/Page/services pages/Design_planning.jsx
@@ -21,149 +21,95 @@ export default function Design_planning() {
   }
   return (
     <>
-    <div>
-      <div className="section1-design-body-cont">
-        <div className="container">
-          <div className="design-cont">
-            <div className="row">
-              <div className="design-row">
-                <div className="specillay-col-left col-4 order-2  ">
-                  <div className="left-design-cont design-col ">
-                    <div className="left-col-cont">
-                      <div className="left-card">
-                        <div className="left-card-first-cont">
-                          <h2>Our Service</h2>
-                          <ul className="list-cont">
-                            {ServiceData.map((service) => (
-                              <li key={service.id}>
-                                <div className="a-arrow-cont">
-                                  <Link
-                                    to={`/designplanning/${service.id}`}
-                                    data-index-left=""
-                                  >
-                                    {service.title}
-                                  </Link>
-                                  <FaArrowCircleRight className="right-arrow" />
-                                </div>
-                              </li>
-                            ))}
-                          </ul>
+      <div>
+        <div className="section1-design-body-cont">
+          <div className="container">
+            <div className="design-cont">
+              <div className="row">
+                <div className="design-row">
+                  <div className="specillay-col-left col-4 order-2  ">
+                    <div className="left-design-cont design-col ">
+                      <div className="left-col-cont">
+                        <div className="left-card">
+                          <div className="left-card-first-cont">
+                            <h2>Our Service</h2>
+                            <ul className="list-cont">
+                              {ServiceData.map((service) => (
+                                <li key={service.id}>
+                                  <div className="a-arrow-cont">
+                                    <Link
+                                      to={`/designplanning/${service.id}`}
+                                      data-index-left=""
+                                    >
+                                      {service.title}
+                                    </Link>
+                                    <FaArrowCircleRight className="right-arrow" />
+                                  </div>
+                                </li>
+                              ))}
+                            </ul>
+                          </div>
                         </div>
-                      </div>
-                      <div className="left-card-img-cont">
-                        {/* <img
+                        <div className="left-card-img-cont">
+                          {/* <img
                           src="https://img.freepik.com/free-photo/retro-couch-living-room_23-2150711898.jpg?t=st=1723629736~exp=1723633336~hmac=3f22c5a22781deae0c97ac4858a721c8a02fd16650f244da5a95fcaf0d27ae79&w=900"
                           alt="couch"
                         /> */}
-                        <div className="img-overlay"></div>
-                        <div className="text-overlay">
-                          <h4>Ready to start learning?</h4>
-                          <h3>Sign up now!</h3>
-                          <span>
-                            <MdAddCall /> +91 123456789
-                          </span>
-                          <div className="register-btn">
-                            <p>Register Now</p>
+                          <div className="img-overlay"></div>
+                          <div className="text-overlay">
+                            <h4>Ready to start learning?</h4>
+                            <h3>Sign up now!</h3>
+                            <span>
+                              <MdAddCall /> +91 123456789
+                            </span>
+                            <div className="register-btn">
+                              <p>Register Now</p>
+                            </div>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className="specifically-right-col col-8 order-1  ">
-                  {/* <div className="design-col"> */}
-                  <div className="right-col-cont">
-                    <div className="right-card-1 right-card">
-                      <img src={service.img[0]} alt={"banner-img"} />
-                    </div>
-                    <div className="right-card2">
-                      <div className="right-card-2-cont">
-                        <div className="right-card-2-first-div">
-                          <h4>
-                            <h4>{service.title}</h4>{" "}
-                          </h4>
-                        </div>
-                        <div className="right-card-2-second-div">
-                          <p>
-                            <span
-                              style={{
-                                fontSize: 52,
-                                fontWeight: 700,
-                                color: "#BB9A65",
-                                fontFamily: "Space Grotesk",
-                              }}
-                            >
-                              M
-                            </span>
-                            {service.description1}
-                          </p>
-                        </div>
-                        <div className="container">
-                          <div className="right-card2-cont">
-                            <div className="row">
-                              <div className="right-card2-row">
-                                <div className="col-6">
-                                  <div className="right-card2-col-cont">
-                                    <div className="right-card2-col">
-                                      <span>
-                                        {" "}
-                                        <FaCheckCircle /> Experienced,
-                                        time-served engineers{" "}
-                                      </span>
-                                    </div>
-                                    <div className="right-card2-col">
-                                      <span>
-                                        {" "}
-                                        <FaCheckCircle /> Experienced,
-                                        time-served engineers{" "}
-                                      </span>
-                                    </div>
-                                    <div className="right-card2-col">
-                                      <span>
-                                        {" "}
-                                        <FaCheckCircle /> Experienced,
-                                        time-served engineers{" "}
-                                      </span>
-                                    </div>
-                                    <div className="right-card2-col">
-                                      <span>
-                                        {" "}
-                                        <FaCheckCircle /> Experienced,
-                                        time-served engineers{" "}
-                                      </span>
-                                    </div>
+                  <div className="specifically-right-col col-8 order-1  ">
+                    {/* <div className="design-col"> */}
+                    <div className="right-col-cont">
+                      <div className="right-card-1 right-card">
+                        <img src={service.img[0]} alt={"banner-img"} />
+                      </div>
+                      <div className="right-card2">
+                        <div className="right-card-2-cont">
+                          <div className="right-card-2-first-div">
+                            <h4>
+                              <h4>{service.title}</h4>{" "}
+                            </h4>
+                          </div>
+                          <div className="right-card-2-second-div">
+                            <p>
+                              <span
+                                style={{
+                                  fontSize: 52,
+                                  fontWeight: 700,
+                                  color: "#BB9A65",
+                                  fontFamily: "Space Grotesk",
+                                }}
+                              >
+                                M
+                              </span>
+                              {service.description1}
+                            </p>
+                            <p>
+                              {service.description2}
+                            </p>
+                          </div>
+                          <div className="container">
+                            <div className="right-card2-cont">
+                              <div className="row">
+                                <div className="right-card2-row">
+                                  <div className="col-6">
+                                    <div className="right-card2-col-cont"></div>
                                   </div>
-                                </div>
-                                <div className="col-6">
-                                  <div className="right-card2-col-cont">
-                                    <div className="right-card2-col">
-                                      <span>
-                                        {" "}
-                                        <FaCheckCircle /> Experienced,
-                                        time-served engineers{" "}
-                                      </span>
-                                    </div>
-                                    <div className="right-card2-col">
-                                      <span>
-                                        {" "}
-                                        <FaCheckCircle /> Experienced,
-                                        time-served engineers{" "}
-                                      </span>
-                                    </div>
-                                    <div className="right-card2-col">
-                                      <span>
-                                        {" "}
-                                        <FaCheckCircle /> Experienced,
-                                        time-served engineers{" "}
-                                      </span>
-                                    </div>
-                                    <div className="right-card2-col">
-                                      <span>
-                                        {" "}
-                                        <FaCheckCircle /> Experienced,
-                                        time-served engineers{" "}
-                                      </span>
-                                    </div>
+                                  <div className="col-6">
+                                    <div className="right-card2-col-cont"></div>
                                   </div>
                                 </div>
                               </div>
@@ -171,27 +117,26 @@ export default function Design_planning() {
                           </div>
                         </div>
                       </div>
-                    </div>
-                    <div className="right-card">
-                      <div className="service-gallery-cont">
-                        <div className="service-gallery">
-                          <img src={service.img[1]} />
-                        </div>
-                        <div className="service-gallery">
-                          <img src={service.img[2]} />
+                      <div className="right-card">
+                        <div className="service-gallery-cont">
+                          <div className="service-gallery">
+                            <img src={service.img[1]} />
+                          </div>
+                          <div className="service-gallery">
+                            <img src={service.img[2]} />
+                          </div>
                         </div>
                       </div>
                     </div>
+                    {/* </div> */}
                   </div>
-                  {/* </div> */}
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <Footer />
+      <Footer />
     </>
   );
 }
